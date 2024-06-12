7707288e8c4311fbe4f0e7c604e92ce04e5084b5
NASDAMI Kafil <kafil.nasdami@gmail.com>
2024-06-12 23:01:42 +0100

    added changes in product schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,18 +1,29 @@
-import {relations} from 'drizzle-orm';
+import { relations } from 'drizzle-orm';
 import {
-	integer, pgTable, serial, varchar, date,
+	date,
+	integer,
+	pgEnum,
+	pgTable,
 	primaryKey,
+	serial, varchar,
 } from 'drizzle-orm/pg-core';
+
+
 // Declaring enum in database
+const ProductType = pgEnum('type', ['NORMAL', 'SEASONAL','EXPIRABLE',"FLASHSALE"]);
+
 export const products = pgTable('products', {
 	id: serial('id').primaryKey(),
 	leadTime: integer('lead_time').notNull(),
 	available: integer('available').notNull(),
-	type: varchar('type', {length: 256}).notNull(),
+	type: ProductType('type').notNull().default('NORMAL'),
 	name: varchar('name', {length: 256}).notNull(),
 	expiryDate: date('expiry_date', {mode: 'date'}),
 	seasonStartDate: date('season_start_date', {mode: 'date'}),
 	seasonEndDate: date('season_end_date', {mode: 'date'}),
+	flashSaleStart:date('flash_sale_start', {mode: 'date'}),
+	flashSaleEnd:date('flash_sale_end', {mode: 'date'}),
+	maxFlashSaleQuantity:integer('max_flash_sale_quantity').notNull(),
 });
 
 export type Product = typeof products.$inferSelect;
